f71fe21fe55cb274c1c6611acbc712c522ba2415
uxbydlh <159072891+uxbydlh@users.noreply.github.com>
2025-11-28 13:07:39 +0000

    Added logos of companies worked with and created a json list for them

---
+++ _data/companiesWorkedWith.js
@@ -0,0 +1,47 @@
+[
+	{
+		"name": "Adenydd",
+		"logo": "logo-adenydd.png",
+		"link": "https://adenydd.org/",
+	},
+    {
+		"name": "Devon Healing Horsemanship",
+		"logo": "logo-DHH.png",
+		"link": "#WIP",
+	},
+    {
+		"name": "Dot Dot Loans",
+		"logo": "logo-dotdot.svg",
+		"link": "#WIP",
+	},
+    {
+		"name": "Eco Centre CBS Limited",
+		"logo": "logo-EcoCentre.png",
+		"link": "https://www.ecocentresw.org/",
+	},
+    {
+		"name": "Give and Grow CIC",
+		"logo": "logo-GiveGrow.png",
+		"link": "#WIP",
+	},
+    {
+		"name": "Morses Club",
+		"logo": "logo-morses.svg",
+		"link": "#WIP",
+	},
+    {
+		"name": "NCR Atleos",
+		"logo": "logo-ncr.svg",
+		"link": "#WIP",
+	},
+    {
+		"name": "Oakbrook Finance Ltd",
+		"logo": "logo-oakbrook.svg",
+		"link": "#WIP",
+	},
+    {
+		"name": "Very Evil Demons",
+		"logo": "logo-VED.webp",
+		"link": "https://veryevildemons.com/",
+	},
+]
